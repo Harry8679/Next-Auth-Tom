91177ab10dec328fc6c72d0d7062fde729e5e6af
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-26 15:05:47 +0200

    Create the Register form using zod and react-hook-form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,36 @@
+'use client';
+
+import { z } from 'zod';
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useForm } from 'react-hook-form/dist';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().email(),
+    passwordConfirm: z.string(),
+});
+
 export default function Register() {
-    return <div>Register Page </div>;
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+            passwordConfirm: '',
+        }
+    });
+
+    const handleSubmit = async(data: z.infer<typeof formSchema>) => {}
+
+    return (
+        <main className="flex justify-center items-center min-h-screen">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Register</CardTitle>
+                    <CardDescription>Register for a new account.</CardDescription>
+                </CardHeader>
+            </Card>
+        </main>
+    );
 }
